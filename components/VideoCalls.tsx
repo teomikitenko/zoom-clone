2d3254439e3cbc44438e7e72e7b76e2a80f33df5
Yevhenii <jenmikitenko@gmail.com>
2024-05-08 21:39:10 +0300

    test video calls v2

--- components/VideoCalls.tsx
+++ components/VideoCalls.tsx
@@ -28,7 +28,7 @@ const VideoCalls = () => {
   useEffect(() => {
     if (isLoaded) {
       const tokenProvider = async () => {
-        const res = await fetch(`/api/generate_token`, { cache: "no-store" });
+        const res = await fetch(`${process.env.NEXT_PUBLIC_MY_DEPLOYING_URL}/api/generate_token`, { cache: "no-store" });
         const { token } = await res.json();
         return token;
       };
