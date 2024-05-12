7b316ea2be50e4387b49271739d741a7a7f70469
Yevhenii <jenmikitenko@gmail.com>
2024-05-12 12:11:59 +0300

    add clerk  webhook4

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,13 @@ import { clerkMiddleware,createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher([
   '/(.*)',
 ]);
+const isWebhook = createRouteMatcher([
+  '/api/webhook_clerk/(.*)',
+  '/api/generate_token/(.*)'
+]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  if (isProtectedRoute(req)&&!isWebhook(req)) auth().protect();
 });
 
 export const config = {
