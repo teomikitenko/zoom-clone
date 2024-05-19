fbbe58efc485cd617a6c851b1a117193f73fc87b
Yevhenii <jenmikitenko@gmail.com>
2024-05-19 21:35:20 +0300

    rework logic to get call instance

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -15,27 +15,24 @@ const Provider = ({ children }: { children: React.ReactNode }) => {
   
   useEffect(() => {
     if (isLoaded) {
-      const f = ()=>{
-        const tokenProvider = async () => {
-          const res = await fetch(`${process.env.NEXT_PUBLIC_MY_DEPLOYING_URL}/api/generate_token`,{
-            method:'POST'
-          });
-          const { token } = await res.json();
-          return token;
-        };
-        const user: User = {
-          id: authUser?.id as string,
-          name: authUser?.firstName as string,
-          image: authUser?.imageUrl,
-        };
-        const client = new StreamVideoClient({
-          apiKey: process.env.NEXT_PUBLIC_STREAM_API_KEY as string,
-          user,
-          tokenProvider: tokenProvider as TokenProvider,
+      const tokenProvider = async () => {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_MY_DEPLOYING_URL}/api/generate_token`,{
+          method:'POST'
         });
-        setMyClient(client);
-      }
-    setTimeout(f,2000)
+        const { token } = await res.json();
+        return token;
+      };
+      const user: User = {
+        id: authUser?.id as string,
+        name: authUser?.firstName as string,
+        image: authUser?.imageUrl,
+      };
+      const client = new StreamVideoClient({
+        apiKey: process.env.NEXT_PUBLIC_STREAM_API_KEY as string,
+        user,
+        tokenProvider: tokenProvider as TokenProvider,
+      });
+      setMyClient(client);
     }
   }, [isLoaded]);
 
